1ee9ef68bc40b5c9352cfdb007fb066ee6da72d3
Antoine-Ali Zarrouk <antoineali.zarrouk@viacesi.fr>
2013-11-07 14:00:10 +0100

    Deleted unused imports on BookFacade Created ManagedBeans

--- booXstore/booXstore-ejb/src/main/java/com/booXstore/service/BookFacade.java
+++ booXstore/booXstore-ejb/src/main/java/com/booXstore/service/BookFacade.java
@@ -5,22 +5,11 @@
 package com.booXstore.service;
 
 import com.booXstore.domain.Book;
-import com.booXstore.domain.BookOrders;
-import com.booXstore.domain.Orders;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
-import org.eclipse.persistence.internal.queries.OrderedListContainerPolicy;
 
 /**
  *
