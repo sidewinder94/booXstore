e7404ada83966b53b17cebf9c631266f1bb7214b
Killium <benjamin.robert@viacesi.fr>
2013-11-08 14:22:01 +0100

    Update JSF Management Gestion Livres Left Nav + Create CategoriesBean

---
+++ booXtore/booXtore-war/src/java/com/booXtore/CategoriesBean.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.booXtore;
+
+import javax.inject.Named;
+import javax.enterprise.context.Dependent;
+
+/**
+ *
+ * @author brobert
+ */
+@Named(value = "categoriesBean")
+@Dependent
+public class CategoriesBean {
+
+    /**
+     * Creates a new instance of CategoriesBean
+     */
+    public CategoriesBean() {
+    }
+}
