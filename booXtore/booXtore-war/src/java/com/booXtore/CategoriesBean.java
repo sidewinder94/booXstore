cc5edee8db55f1cab3e2f3a67c38f560bce3d68b
Killium <benjamin.robert@viacesi.fr>
2013-11-08 15:18:17 +0100

    Listing catégories

--- booXtore/booXtore-war/src/java/com/booXtore/CategoriesBean.java
+++ booXtore/booXtore-war/src/java/com/booXtore/CategoriesBean.java
@@ -4,8 +4,17 @@
  */
 package com.booXtore;
 
+import com.booXtore.domain.Categories;
+import com.booXtore.service.BooksFacadeLocal;
+import com.booXtore.service.CategoriesFacadeLocal;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.ManagedBean;
+import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.Dependent;
+import javax.faces.context.FacesContext;
 
 /**
  *
@@ -15,9 +24,14 @@ import javax.enterprise.context.Dependent;
 @Dependent
 public class CategoriesBean {
 
-    /**
-     * Creates a new instance of CategoriesBean
-     */
-    public CategoriesBean() {
+    @EJB
+    private BooksFacadeLocal bFL;
+    
+    @EJB
+    private CategoriesFacadeLocal cFL;
+    
+    public List<Categories> getCategories()
+    {
+        return cFL.findAll();
     }
 }
